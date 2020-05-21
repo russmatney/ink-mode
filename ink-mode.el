5d331f95d1a291bd20ecda668bf7da949915b6c4
Damien Picard <dam.pic@free.fr>
2020-05-22 00:13:35 +0200

    Update commentary and keywords; bump version

--- ink-mode.el
+++ ink-mode.el
@@ -5,15 +5,16 @@
 
 ;; Author: Erik Sjöstrand
 ;; URL: http://github.com/Kungsgeten/ink-mode
-;; Version: 0.1
-;; Keywords: languages
+;; Version: 0.2
+;; Keywords: languages, wp, hypermedia
 ;; Package-Requires: ((emacs "24.3"))
 
 ;;; Commentary:
 
-;; `ink-mode' provides basic syntax highlightning for the Ink scripting
-;; language, developed by Inkle Studios.  There's also a command `ink-play' to
-;; playtest your story from Emacs (bound to C-c C-c by default).
+;; `ink-mode' provides basic syntax highlighting and indentation for
+;; the Ink scripting language, developed by Inkle Studios. There's
+;; also a command `ink-play' to playtest your story from Emacs (bound
+;; to C-c C-c by default).
 
 ;;; Code:
 (require 'rx)
