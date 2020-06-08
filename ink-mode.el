f883fefb739d62e75a3f7247ea5c6ba8a0895c23
Damien Picard <dam.pic@free.fr>
2020-06-08 20:12:37 +0200

    Version

--- ink-mode.el
+++ ink-mode.el
@@ -7,7 +7,7 @@
 ;;         Damien Picard
 ;; Maintainer: Damien Picard
 ;; URL: http://github.com/Kungsgeten/ink-mode
-;; Version: 0.2.1
+;; Version: 0.3.0
 ;; Keywords: languages, wp, hypermedia
 ;; Package-Requires: ((emacs "26.1"))
 
